f82d0f755910d1e2c48c898a754dd392efc7ccee
Vu Le <tuongvu@gmail.com>
2020-09-17 21:35:56 +0700

    Fix test failure.

--- pipes/gen_test.go
+++ pipes/gen_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/finderseyes/piper/pipes/mocks"
+	_ "github.com/finderseyes/piper/testing"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
